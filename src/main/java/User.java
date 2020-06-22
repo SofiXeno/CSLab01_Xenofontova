c6ed353c0527bc290590edbbc81e3fcd727841e8
Sofi Xeno <46944287+SofiXeno@users.noreply.github.com>
2020-06-22 15:29:37 +0300

    init commit

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -18,15 +18,15 @@ public class User {
 
 
 
-//    private static int freeUserID = 0;
-//
-//    public int getUserID() {
-//        return userID;
-//    }
-//
-//    private final int userID;
-//
-//    public User(){
-//        userID = freeUserID++;
-//    }
+    private static int freeUserID = 0;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    private final int userID;
+
+    public User(){
+        userID = freeUserID++;
+    }
 }
